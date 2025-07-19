8d84fc57dc545cde1a819c70298ec298096434ef
Ngo Phuoc Loi <phuocloi11223@gmail.com>
2025-07-19 09:40:21 +0700

    ZEN-10: Config interceptor logging

---
+++ src/main/java/com/nploi/zentrix/WebConfig.java
@@ -0,0 +1,20 @@
+package com.nploi.zentrix;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.nploi.zentrix.interceptor.LoggingInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoggingInterceptor loggingInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loggingInterceptor);
+    }
+}
